3fbe34b71ea099d95ab312587edf0424e60b78f9
Mateus Moura Domingos <mateus.md1902@gmail.com>
2022-08-02 14:12:18 -0300

    update

--- cap03/js/resp3_c.js
+++ cap03/js/resp3_c.js
@@ -13,23 +13,31 @@ function mostrarTempo() {
         return;
     }
 
-    if (valor > 3) {
+    /*
+    Valor | tempo
+     1,00 |  30 min
+     1,75 |  60 min
+     3,00 |  120 min
+    */
+
+    // estrutura de condições do paquinmetro
+    if (valor > 3) {  // se tentar pagar mais que 3 reais
         alert("O valor máximo é R$ 3.00");
         return;
-    } else if (valor == 3) {
+    } else if (valor == 3) {  // se pagar 3 reais
         tempo = 120;
         troco = valor - 3;
         outTempo.textContent = "Tempo: " + tempo + " min";
         outTroco.textContent = "";
-    } else if (valor >= 1.75) {
+    } else if (valor >= 1.75) {  // se pagar entre 1.75 e 3 reais
         tempo = 60;
         troco = valor - 1.75;
         outTempo.textContent = "Tempo: " + tempo + " min";
-        if (troco == 0) {
+        if (troco == 0) { // se o troco for 0
             outTroco.textContent = "";
-        } else if (troco < 1) {
+        } else if (troco < 1) { // se o troco for menor que 1
             outTroco.textContent = "Troco R$: " + troco.toFixed(2);
-        } else {
+        } else { // se o troco for maior que 1 ( somar 1.75 + 1 e calcular o troco)
             tempo = 90;
             troco = troco - 1;
             outTempo.textContent = "Tempo: " + tempo + " min";
@@ -39,7 +47,7 @@ function mostrarTempo() {
                 outTroco.textContent = "Troco R$: " + troco.toFixed(2);
             }
         }
-    } else if (valor >= 1) {
+    } else if (valor >= 1) { // se pagar entre 1 e 1.75 reais
         tempo = 30;
         troco = valor - 1
         outTempo.textContent = "Tempo: " + tempo + " min";
@@ -48,7 +56,7 @@ function mostrarTempo() {
         } else {
             outTroco.textContent = "Troco R$: " + troco.toFixed(2);
         }
-    } else {
+    } else { // se pagar menos que 1 real
         alert("Valor insuficiente");
         inValor.focus();
         return;
